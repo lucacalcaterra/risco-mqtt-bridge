0dae214546a77a9eb3e6610ca40d9babc65cf240
Luca Calcaterra <calcaterra.luca@gmail.com>
2018-10-29 21:17:21 +0100

    change logs dir

--- logger.js
+++ logger.js
@@ -22,8 +22,8 @@ const riscoLogger = createLogger({
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write all logs error (and below) to `error.log`.
     //
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
