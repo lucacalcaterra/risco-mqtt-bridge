dd24409527b11882484f711efb8b8f9900184c75
Luca Calcaterra <calcaterra.luca@gmail.com>
2019-02-12 19:49:27 +0100

    Ok, ready for testing. (probably need code cleanup)

--- appnew.js
+++ appnew.js
@@ -1,11 +1,22 @@
 /* eslint linebreak-style: ["error", "windows"] */
-const MqttHABridge = require('./mqttHABridge');
-const riscoLogger = require('./logger');
 
-const bridge = new MqttHABridge(8000, { host: 'localhost' });
+/** Risco MQTT Bridge
+ * @author Luca Calcaterra <calcaterra.luca@gmail.com>
+ * @version 1.5 Beta
+ */
+
+const MqttHABridge = require('./mqttHABridge');
+const logger = require('./logger');
+const Config = require('./config/config');
+
+const bridgeMQTT = new MqttHABridge(Config.Conn.POLLINGINTERVAL, {
+  host: Config.Mqtt.url.MQTT_SERVER,
+  port: Config.Mqtt.url.MQTT_PORT,
+  options: Config.Mqtt.options
+});
 
 process.on('SIGINT', async () => {
-  riscoLogger.log('info', 'Exiting ... ');
-  await bridge.stop();
+  logger.log('info', 'Exiting ... ');
+  await bridgeMQTT.stop();
   process.exit();
 });
--- mqttHABridge.js
+++ mqttHABridge.js
@@ -6,22 +6,27 @@ const logger = require('./logger');
 const config = require('./config/config');
 
 const BASE_PREFIX = 'homeassistant';
-const HA_MQTT_COMPONENT = 'alarm_control_panel';
 const GROUP_ID = 'risco';
-const NODE_ID = 'alarmpanel';
-const STATE_TOPIC = `${BASE_PREFIX}/${HA_MQTT_COMPONENT}/${GROUP_ID}/${NODE_ID}`;
-const COMMAND_TOPIC = `${STATE_TOPIC}/set`;
-const AVAILABILITY_TOPIC = `${STATE_TOPIC}/available`;
 
+// ** Panel
+const HAMQTT_PANEL_COMPONENT = 'alarm_control_panel';
+const NODE_PANEL_ID = 'RiscoPanel';
+const STATE_PANEL_TOPIC = `${BASE_PREFIX}/${HAMQTT_PANEL_COMPONENT}/${GROUP_ID}/${NODE_PANEL_ID}`;
+const COMMAND_PANEL_TOPIC = `${STATE_PANEL_TOPIC}/set`;
+const AVAILABILITY_PANEL_TOPIC = `${STATE_PANEL_TOPIC}/available`;
+// ** Sensors
+const HAMQTT_DETECTOR_COMPONENT = 'sensor';
+const NODE_DETECTOR_ID = 'RiscoDetector';
+const STATE_DETECTOR_TOPIC = `${BASE_PREFIX}/${HAMQTT_DETECTOR_COMPONENT}/${GROUP_ID}/${NODE_DETECTOR_ID}`;
 const MQTT_PANEL_CONFIG = {
-  CONFIG_TOPIC: `${STATE_TOPIC}/config`,
+  CONFIG_TOPIC: `${STATE_PANEL_TOPIC}/config`,
   // Config Message for Autodiscovery
   CONFIG_MSG: {
-    name: NODE_ID,
-    unique_id: NODE_ID,
-    state_topic: STATE_TOPIC,
-    command_topic: COMMAND_TOPIC,
-    availability_topic: AVAILABILITY_TOPIC,
+    name: `${NODE_PANEL_ID} - Partition 1`,
+    unique_id: NODE_PANEL_ID,
+    state_topic: STATE_PANEL_TOPIC,
+    command_topic: COMMAND_PANEL_TOPIC,
+    availability_topic: AVAILABILITY_PANEL_TOPIC,
     retain: 'true'
   },
   MSG_OPTIONS: {
@@ -50,20 +55,34 @@ module.exports = class HAMqttBridge extends PanelPoller {
   }
 
   mqttInit() {
+    logger.log('info', 'Waiting for connection to MQTT...');
     this.mqttClient = mqtt.connect(this.mqttConn);
+    // on error
+    this.mqttClient.on('error', err => {
+      logger.log('error', `error! Cannot connect to MQTT Server: ${err}`);
+    });
     // on connect
     this.mqttClient.on('connect', async () => {
       logger.log('info', 'Connected to MQTT Server');
       // subscribe to topics
-      this.mqttClient.subscribe(`${STATE_TOPIC}/set`);
+      this.mqttClient.subscribe(`${STATE_PANEL_TOPIC}/set`);
       try {
         if (await this.init()) {
-          this.mqttClient.publish(AVAILABILITY_TOPIC, 'online', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+          this.mqttClient.publish(
+            AVAILABILITY_PANEL_TOPIC,
+            'online',
+            MQTT_PANEL_CONFIG.MSG_OPTIONS
+          );
+          this.publishDetectors();
           await this.start();
         } else
-          this.mqttClient.publish(AVAILABILITY_TOPIC, 'offline', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+          this.mqttClient.publish(
+            AVAILABILITY_PANEL_TOPIC,
+            'offline',
+            MQTT_PANEL_CONFIG.MSG_OPTIONS
+          );
       } catch (error) {
-        this.mqttClient.publish(AVAILABILITY_TOPIC, 'offline', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+        this.mqttClient.publish(AVAILABILITY_PANEL_TOPIC, 'offline', MQTT_PANEL_CONFIG.MSG_OPTIONS);
         logger.log(
           'error',
           'MQTT bridge not working because the panel is offline (check init sequence)'
@@ -81,61 +100,93 @@ module.exports = class HAMqttBridge extends PanelPoller {
 
       // ************* when connected publish messages from panel
       this.on('polled', () => {
-        this.mqttClient.publish(AVAILABILITY_TOPIC, 'online', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+        this.mqttClient.publish(AVAILABILITY_PANEL_TOPIC, 'online', MQTT_PANEL_CONFIG.MSG_OPTIONS);
       });
       // when new panel status arrives
       this.on('newpanelstatus', () => {
         if (this.riscoConn.riscoArmStatus !== null) {
           this.mqttClient.publish(
-            STATE_TOPIC,
+            STATE_PANEL_TOPIC,
             MQTT_PANEL_CONFIG.STATES[this.riscoConn.riscoArmStatus],
             MQTT_PANEL_CONFIG.MSG_OPTIONS
           );
+          // call publish detectors info
+          this.publishDetectors();
+          // publish eventhistory
+          if (this.riscoConn.riscoEventHistory !== null) {
+            this.mqttClient.publish(
+              `${STATE_PANEL_TOPIC}/eventhistory`,
+              JSON.stringify(this.riscoConn.riscoEventHistory),
+              MQTT_PANEL_CONFIG.MSG_OPTIONS
+            );
+          }
         }
       });
       this.on('stopped', () => {
-        this.mqttClient.publish(AVAILABILITY_TOPIC, 'offline', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+        this.mqttClient.publish(AVAILABILITY_PANEL_TOPIC, 'offline', MQTT_PANEL_CONFIG.MSG_OPTIONS);
+      });
+
+      // when new message arrives
+      this.mqttClient.on('message', (topic, message) => {
+        logger.log('info', `message from mqtt arrived:${topic}/${message}`);
+
+        switch (topic) {
+          case COMMAND_PANEL_TOPIC:
+            switch (message.toString()) {
+              case MQTT_PANEL_CONFIG.ARM_COMMANDS.ARM:
+                this.riscoConn.setArm('armed');
+                this.mqttClient.publish(STATE_PANEL_TOPIC, 'pending');
+                logger.log('info', 'command arrived: ARM');
+                break;
+              case MQTT_PANEL_CONFIG.ARM_COMMANDS.DISARM:
+                this.riscoConn.setArm('disarmed');
+                this.mqttClient.publish(STATE_PANEL_TOPIC, 'pending');
+                logger.log('info', 'command arrived: DISARM');
+                break;
+              case MQTT_PANEL_CONFIG.ARM_COMMANDS.PARTARM:
+                this.mqttClient.publish(STATE_PANEL_TOPIC, 'pending');
+                this.riscoConn.setArm('partially');
+                logger.log('info', 'command arrived: PARTARM');
+                break;
+              default:
+                logger.log('warn', 'arm command not recognized');
+            }
+            break;
+          default:
+            logger.log('warn', '...command not recognized');
+        }
+      });
+
+      this.mqttClient.on('close', () => {
+        logger.log('info', 'connection to MQTT closed');
+        this.stop();
       });
     });
+  }
 
-    // when new message arrives
-    this.mqttClient.on('message', (topic, message) => {
-      logger.log('info', `message from mqtt arrived:${topic}/${message}`);
+  publishDetectors() {
+    const detectorsArray = this.riscoConn.riscoDetectors.parts[0].detectors;
+    if (detectorsArray !== null) {
+      logger.log('info', 'Publish detectors info');
 
-      switch (topic) {
-        case COMMAND_TOPIC:
-          switch (message.toString()) {
-            case MQTT_PANEL_CONFIG.ARM_COMMANDS.ARM:
-              this.riscoConn.setArm('armed');
-              this.mqttClient.publish(STATE_TOPIC, 'pending');
-              logger.log('info', 'command arrived: ARM');
-              break;
-            case MQTT_PANEL_CONFIG.ARM_COMMANDS.DISARM:
-              this.riscoConn.setArm('disarmed');
-              this.mqttClient.publish(STATE_TOPIC, 'pending');
-              logger.log('info', 'command arrived: DISARM');
-              break;
-            case MQTT_PANEL_CONFIG.ARM_COMMANDS.PARTARM:
-              this.mqttClient.publish(STATE_TOPIC, 'pending');
-              this.riscoConn.setArm('partially');
-              logger.log('info', 'command arrived: PARTARM');
-              break;
-            default:
-              logger.log('warn', 'arm command not recognized');
-          }
-          break;
-        default:
-          logger.log('warn', '...command not recognized');
-      }
-    });
-
-    this.mqttClient.on('close', () => {
-      logger.log('info', 'connection to MQTT closed');
-    });
-
-    // on error
-    this.mqttClient.on('error', err => {
-      logger.log('error', `error! Cannot connect to MQTT Server: ${err}`);
-    });
+      // cycle array
+      detectorsArray.forEach(element => {
+        const decttopic = STATE_DETECTOR_TOPIC + element.id;
+        const configMsg = {
+          name: `Dect-${element.name}`,
+          state_topic: `${decttopic}/state`
+        };
+        this.mqttClient.publish(`${decttopic}/config`, JSON.stringify(configMsg), {
+          retain: true
+        });
+        let state = false;
+        if (element.filter === '') state = 'OFF';
+        else if (element.filter === 'triggered') state = 'ON';
+        else if (element.filter === 'bypassed') state = 'BYP';
+        this.mqttClient.publish(`${decttopic}/state`, state, {
+          retain: true
+        });
+      });
+    }
   }
 };
